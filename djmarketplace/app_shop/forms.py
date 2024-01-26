b5116cea5eb4beafda077f968b6142d732f6a0fd
Lobanova Anastasia < - 6@DESKTOP-0GFIBIO>
2024-01-26 20:29:35 +0300

    lesson_3 done

--- djmarketplace/app_shop/forms.py
+++ djmarketplace/app_shop/forms.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.forms import UserCreationForm
-from .models import User
+from .models import User, GoodCart
 from django import forms
 
 class UserForm(UserCreationForm):
@@ -9,6 +9,12 @@ class UserForm(UserCreationForm):
                   'password1', 'password2')
 
 
+
+class CartAddForm(forms.ModelForm):
+
+    class Meta:
+        model = GoodCart
+        fields = ('good_num', )
 
 
 
